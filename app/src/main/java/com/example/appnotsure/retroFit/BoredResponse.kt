147e63dd2f734762b3072d6296ef56dc1adc3ce7
Leonardo Daniel Perez Fiorentino <leoperez@ar0c02g44wwml85.ml.com>
2021-12-29 18:08:56 -0300

    Added the retrofit classes

---
+++ app/src/main/java/com/example/appnotsure/retroFit/BoredResponse.kt
@@ -0,0 +1,9 @@
+package com.example.appnotsure.retroFit
+
+data class BoredResponse(
+    var activity: String,
+    var accessibility: Double,
+    var type: String,
+    var participants: Int,
+    var price: Double,
+    var key: String)

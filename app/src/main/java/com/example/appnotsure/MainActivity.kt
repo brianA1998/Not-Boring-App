e7b97ecf45916bcc604d0065e87f20af99922849
brianA1998 <brian.alaniz@mercadolibre.com>
2022-01-03 09:58:31 -0300

    ** Added ** - Navigate activity to Fragment terms and conditions - Change ConstraintLayout to RelativeLayout

--- app/src/main/java/com/example/appnotsure/MainActivity.kt
+++ app/src/main/java/com/example/appnotsure/MainActivity.kt
@@ -4,18 +4,22 @@ import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.example.appnotsure.databinding.ActivityMainBinding
 import java.time.Duration
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setupActivityLink()
         validationParticipants()
 
@@ -26,8 +30,8 @@ class MainActivity : AppCompatActivity() {
      * Invalid number for example -1 or 0
      */
     private fun validationParticipants() {
-        val button = findViewById<Button>(R.id.button_start)
-        val inputText = findViewById<EditText>(R.id.edit_text_participants)
+        val button = binding.buttonStart
+        val inputText = binding.editTextParticipants
         inputText.error = null
         button.setOnClickListener {
             val numberParticipants: Int = Integer.parseInt(inputText.text.toString())
@@ -38,17 +42,28 @@ class MainActivity : AppCompatActivity() {
                 //Navecion a la pantalla de activities
                 Toast.makeText(this, "Todo correcto", Toast.LENGTH_SHORT).show()
             }
-
-
         }
     }
 
-
+    /**
+     * Navigation settings the activity to fragment
+     */
     private fun setupActivityLink() {
-        val linkTextView = findViewById<TextView>(R.id.activity_intent_navigation_link)
+        val linkTextView = binding.activityIntentNavigationLink
         linkTextView.setOnClickListener {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, TermsAndConditionsFragment()).commit()
+                .replace(R.id.container_fragment, TermsAndConditionsFragment())
+                .commit()
+            hideComponents()
         }
+    }
+
+    /**
+     * Hide activity components in the fragment
+     */
+    private fun hideComponents() {
+        binding.buttonStart.visibility = View.GONE
+        binding.editTextParticipants.visibility = View.GONE
+        binding.tvTitle.visibility = View.GONE
     }
 }
--- app/src/main/java/com/example/appnotsure/TermsAndConditionsFragment.kt
+++ app/src/main/java/com/example/appnotsure/TermsAndConditionsFragment.kt
@@ -1,28 +1,31 @@
 package com.example.appnotsure
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.appnotsure.databinding.FragmentTermsAndConditionsBinding
 
 
 class TermsAndConditionsFragment : Fragment() {
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
-    }
+    private lateinit var binding: FragmentTermsAndConditionsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_terms_and_conditions, container, false)
+        binding = FragmentTermsAndConditionsBinding.inflate(inflater, container, false)
+        val buttonClose = binding.buttonClose
+
+        buttonClose.setOnClickListener {
+            val intent = Intent(requireActivity(), MainActivity::class.java)
+            startActivity(intent)
+        }
+        return binding.root
     }
+
 
 }
